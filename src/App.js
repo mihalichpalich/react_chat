e76d76d5dc3244dc29ffc8cc7b570c41fbaa4c9a
mihalichpalich <pavlovtfspb@gmail.com>
2020-01-18 11:47:06 +0300

    еще поработал немного

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React from "react";
 import "./App.css";
 
+const messages = [
+  {
+    id: 1,
+    text: 'hi',
+    author: 'Ben',
+    channel_id: 1
+  },
+  {
+    id: 2,
+    text: 'hi to you too',
+    author: 'Jen',
+    channel_id: 1
+  },
+  {
+    id: 3,
+    text: 'hi from another user',
+    author: 'Meg',
+    channel_id: 2
+  },
+  {
+    id: 4,
+    text: 'hi to you too from another user',
+    author: 'Jeff',
+    channel_id: 2
+  }
+];
+
+const Message = ({author, text}) => {
+    return (<div className="Message">
+                <div className="Message-author">{author}</div>
+                <div className="Message-text">{text}</div>
+            </div>)
+}
+
+const List = ({messages}) => {
+    return (<div className="MessagePane-Form">
+                {messages.map(({id, author, text}) => <Message key={id} author={author} text={text}/> )}
+            </div>)
+}
+
 function App() {
   return (
     <div className="App">
-        <div className="Message">
-            <div className="Message-text">hi</div>
-        </div>
+        <List messages={messages}/>
     </div>
   );
 }
---
+++ src/seed_data.js
@@ -0,0 +1,33 @@
+const messages = [
+  {
+    id: 1,
+    text: 'hi',
+    author: 'Ben',
+    channel_id: 1
+  },
+  {
+    id: 2,
+    text: 'hi to you too',
+    author: 'Jen',
+    channel_id: 1
+  },
+  {
+    id: 3,
+    text: 'hi from another user',
+    author: 'Meg',
+    channel_id: 2
+  },
+  {
+    id: 4,
+    text: 'hi to you too from another user',
+    author: 'Jeff',
+    channel_id: 2
+  }
+];
+
+const users = [
+  { id: 1, name: 'Ben' },
+  { id: 2, name: 'Jen' },
+  { id: 3, name: 'Meg' },
+  { id: 4, name: 'Jeff' },
+];
