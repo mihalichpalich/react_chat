ab47e0471679e36f603a53e0b08d4010b3977eb1
mihalichpalich <pavlovtfspb@gmail.com>
2020-01-21 13:23:07 +0300

    Updates

---
+++ precache-manifest.575a3a6928603eac2643f25f00706a72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e998c9324e945d90cb854154d05b01",
+    "url": "/react_chat/index.html"
+  },
+  {
+    "revision": "91fb41a83e650cbc754c",
+    "url": "/react_chat/static/css/main.57e8c112.chunk.css"
+  },
+  {
+    "revision": "76cb3c9d1a486f98d6db",
+    "url": "/react_chat/static/js/2.b36f54a9.chunk.js"
+  },
+  {
+    "revision": "25ab7760a9d196e1bee67b6bdb56e1bd",
+    "url": "/react_chat/static/js/2.b36f54a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91fb41a83e650cbc754c",
+    "url": "/react_chat/static/js/main.b31a2e37.chunk.js"
+  },
+  {
+    "revision": "040201b78a9436dabfc9",
+    "url": "/react_chat/static/js/runtime-main.64df385c.js"
+  }
+]);
